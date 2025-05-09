4065f820cfe2ba15616e7ec25e8654770645c7ca
Daniel <daniel.work.co@gmail.com>
2025-05-09 11:19:03 -0400

    init: basic project

---
+++ src/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.DatabaseServices;
+using System;
+
+[assembly: CommandClass(typeof(TNPPlugin.Plugin))]
+
+namespace TNPPlugin
+{
+    public class Plugin: IExtensionApplication
+    {
+        public void Initialize(){}
+
+        public void Terminate() {}
+    }
+}
---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TNPPlugin")]
+[assembly: AssemblyDescription("Civil 3D 2026 Viewport Logger Plugin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("YourCompany")]
+[assembly: AssemblyProduct("TNPPlugin for Civil 3D 2026")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Not visible to COM
+[assembly: ComVisible(false)]
+
+// GUID for COM interop (leave as-is unless needed)
+[assembly: Guid("9df999ce-ff21-4c1e-97dd-4da2159bbb41")]
+
+// Assembly version
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
