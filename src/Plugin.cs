582248dff10dd14cc04b04093e17ce986448bc90
Daniel <daniel.work.co@gmail.com>
2025-05-10 12:18:38 -0400

    feat: sync view port change

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -12,86 +12,109 @@ namespace TNPPlugin
     public class Plugin : IExtensionApplication
     {
         private bool isFirstLoad = true;
-        private Vector2d centerPointOffset = new();
-        private double heightOffset = 0;
-        private double widthOffset = 0;
         private Point2d lastCenterPoint = new();
         private double lastHeight = 0;
         private double lastWidth = 0;
 
         public void Initialize()
         {
-            Application.DocumentManager.MdiActiveDocument.ViewChanged += OnViewChanged;
-            Application.DocumentManager.MdiActiveDocument.LayoutSwitched += OnLayoutSwitched;
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            if (doc != null)
+            {
+                doc.ViewChanged += OnViewChanged;
+                doc.LayoutSwitched += OnLayoutSwitched;
+            }
         }
 
         public void Terminate()
         {
-            Application.DocumentManager.MdiActiveDocument.ViewChanged -= OnViewChanged;
-            Application.DocumentManager.MdiActiveDocument.LayoutSwitched -= OnLayoutSwitched;
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            if (doc != null)
+            {
+                doc.ViewChanged -= OnViewChanged;
+                doc.LayoutSwitched -= OnLayoutSwitched;
+            }
         }
 
         public void OnViewChanged(object sender, EventArgs e)
         {
-            Document doc = Application.DocumentManager.MdiActiveDocument;
-            Editor ed = doc.Editor;
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            var ed = doc.Editor;
 
-            using (ViewTableRecord view = ed.GetCurrentView())
-            {
-                string currentLayout = LayoutManager.Current.CurrentLayout;
-                if (currentLayout == "Model")
-                {
-                    isFirstLoad = false;
-                    centerPointOffset += view.CenterPoint - lastCenterPoint;
-                    heightOffset += view.Height - lastHeight;
-                    widthOffset += view.Width - lastWidth;
-                    lastCenterPoint = view.CenterPoint;
-                    lastHeight = view.Height;
-                    lastWidth = view.Width;
-                }
-            }
+            using var view = ed.GetCurrentView();
+            string layout = LayoutManager.Current.CurrentLayout;
+
+            if (layout != "Model") return;
+
+            isFirstLoad = false;
+
+            lastCenterPoint = view.CenterPoint;
+            lastHeight = view.Height;
+            lastWidth = view.Width;
         }
 
         public void OnLayoutSwitched(object sender, EventArgs e)
         {
             if (isFirstLoad) return;
 
-            Document doc = Application.DocumentManager.MdiActiveDocument;
-            Editor ed = doc.Editor;
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            var ed = doc.Editor;
 
-            using (ViewTableRecord view = ed.GetCurrentView())
+            using var view = ed.GetCurrentView();
+            string layout = LayoutManager.Current.CurrentLayout;
+
+            if (layout == "Model")
             {
-                string currentLayout = LayoutManager.Current.CurrentLayout;
-                if (currentLayout == "Model")
-                {
-                    centerPointOffset = new Vector2d(0, 0);
-                    heightOffset = 0;
-                    widthOffset = 0;
-                    lastCenterPoint = view.CenterPoint;
-                    lastHeight = view.Height;
-                    lastWidth = view.Width;
-                }
+                lastCenterPoint = view.CenterPoint;
+                lastHeight = view.Height;
+                lastWidth = view.Width;
+                return;
+            }
 
-                if (currentLayout.Contains("Calculations"))
-                {
-                    ed.WriteMessage("\nSync Action Started.");
-                    ed.WriteMessage($"\n[Current] CenterPoint: {view.CenterPoint}, Height: {view.Height}, Width: {view.Width}");
-                    using (DocumentLock docLock = doc.LockDocument())
-                    {
-                        Editor layoutEditor = Application.DocumentManager.MdiActiveDocument.Editor;
+            if (!layout.Contains("Calculations")) return;
 
-                        using (ViewTableRecord layoutView = layoutEditor.GetCurrentView())
-                        {
-                            layoutView.CenterPoint = view.CenterPoint + centerPointOffset / 100;
-                            layoutView.Height = view.Height + heightOffset / 100;
-                            layoutView.Width = view.Width + widthOffset / 100;
-                            ed.WriteMessage($"\n[New viewport] CenterPoint: {layoutView.CenterPoint}, Height: {layoutView.Height}, Width: {layoutView.Width}");
-                            layoutEditor.SetCurrentView(layoutView);
-                        }
-                    }
-                    ed.WriteMessage("\nSync Action Ended.");
+            ed.WriteMessage("\nSync Action Started.");
+
+            using (var docLock = doc.LockDocument())
+            {
+                AdjustTargetViewport(doc, ed, view);
+            }
+
+            ed.WriteMessage("\nSync Action Ended.");
+        }
+
+        private void AdjustTargetViewport(Document doc, Editor ed, ViewTableRecord modelView)
+        {
+            Database db = doc.Database;
+
+            using var tr = db.TransactionManager.StartTransaction();
+            var layoutDict = (DBDictionary)tr.GetObject(db.LayoutDictionaryId, OpenMode.ForRead);
+
+            if (!layoutDict.Contains("36 Detention Pond Layout and Calculations"))
+            {
+                ed.WriteMessage("\nLayout 'Calculations' not found.");
+                return;
+            }
+
+            var layoutId = layoutDict.GetAt("36 Detention Pond Layout and Calculations");
+            var layout = (Layout)tr.GetObject(layoutId, OpenMode.ForRead);
+            var btr = (BlockTableRecord)tr.GetObject(layout.BlockTableRecordId, OpenMode.ForRead);
+
+            foreach (ObjectId entId in btr)
+            {
+                if (tr.GetObject(entId, OpenMode.ForWrite) is Viewport vp && vp.Number > 1 && !vp.IsErased)
+                {
+                    ed.WriteMessage($"\n[Current] CenterPoint: {vp.ViewCenter}, Height: {vp.ViewHeight}, Width: {vp.CustomScale}");
+
+                    vp.ViewCenter = lastCenterPoint;
+                    vp.ViewHeight = lastHeight;
+
+                    ed.WriteMessage($"\n[Viewport Updated] ViewCenter: {vp.ViewCenter}, ViewHeight: {vp.ViewHeight}, CustomScale: {vp.CustomScale}");
+                    break;
                 }
             }
+
+            tr.Commit();
         }
     }
 }
